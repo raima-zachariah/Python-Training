b5508538a3ca91352bf03b87a01330ddf75eafc0
Raima Zachariah <raima.zachariah@yahoo.co.in>
2019-02-19 17:20:43 +0530

    Sort functions

---
+++ numsort.py
@@ -0,0 +1,16 @@
+runs = [70,110,30,90,40]
+
+if __name__=="__main__":
+    print(runs)
+    runs.sort()
+    print("sorted runs:", runs)
+
+    with open("runs.txt") as fh:
+        runs_from_file = fh.read().split('\n')
+        
+    print("runs from file:",runs_from_file)
+    # results = [int(x) for x in runs_from_file]
+    results = list(map(int, runs_from_file))
+    print(results)
+    results.sort()
+    print("runs from file in sorted order:", results)
